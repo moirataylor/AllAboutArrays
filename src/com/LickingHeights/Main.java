54a06c2a2f38846ca0410f32e43c7c9f5b86416b
moira <moira.taylor.2023@student.lhschools.org>
2019-11-21 09:17:42 -0500

    Pattern 1

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
 
         int[] array = createNumberArray(size, lowestNumber, highestNumber);
+        Pattern1();
 
         
 
@@ -36,6 +37,17 @@ public class Main {
 
         int number = (int) ((Math.random() * (highestNumber - lowestNumber + 1)) + lowestNumber);
         return number;
+    }
+
+    public static void Pattern1(){
+        for(int rows=0;rows < 6; rows++){
+
+            for(int columns=0;columns<6;columns++){
+                System.out.print("*");
+            }
+       System.out.println();
+        }
+
     }
 
 
